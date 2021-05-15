e91da01167fd2bf894464489814a1a4d8ab6661b
royal-lobster <srujangs8@gmail.com>
2021-05-15 23:55:47 +0530

    :tada: Initial commit
    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
-import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
+export default MyApp;
